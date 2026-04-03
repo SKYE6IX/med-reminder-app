2d1365d1aa7aa6603c1cf9ea5be0ab4e678eacd1
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2026-04-03 10:28:02 +0300

    chore: added unistyle library and created tabs files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Protected guard={true}>
+        <Stack.Screen name="(tabs)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={false}>
+        <Stack.Screen name="(authentication)" />
+      </Stack.Protected>
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -13,3 +13,26 @@ export default function Home() {
     </View>
   );
 }
+
+// Folder structure for how we want the app to be.
+// Tabs
+//    Main tabs:
+//  home
+//  my pills
+//  add
+//  refill
+//  settings
+
+//     All of these above will have tabs will have have these rules:
+// Should only be availabale to authorized user.
+// Some of them are going to have more nested pages(screen) and nested modals
+//
+
+// Other pages that won't need to be in the tabs include:
+// Login page -> Forget password page (Nested page) -> code acttivation -> change password
+// Rgister page
+// Premimum action page.
+// Payment gateway page.
+
+// How does onboarding going to fix in in all this, and then to able to also
+// directly navigate it way to a section when user either go to login page or register page.
