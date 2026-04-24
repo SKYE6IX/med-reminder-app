50856728c09cf3a840fa8f16632e2a84a11dcba8
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2026-04-24 11:59:07 +0300

    UI: Added no content ui to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import PlusIcon from "@/component/icons/plus-icon";
+import CustomButton from "@/component/ui/custom-button/custom-button";
 import WeekView from "@/component/ui/week-view";
 import { useThemeColor } from "@/hooks/use-theme-color";
 import { Image } from "expo-image";
@@ -6,8 +8,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   const color = useThemeColor({}, "textPrimary");
+  const mutedColor = useThemeColor({}, "textMuted");
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea} edges={["top"]}>
       <View style={styles.container}>
         <View style={styles.header}>
           <View style={styles.headerProfileContainer}>
@@ -22,12 +25,33 @@ export default function Home() {
         </View>
 
         <WeekView />
+
+        <View style={styles.noContentWrapper}>
+          <Image
+            source={require("@/assets/images/pill-bottle.png")}
+            style={styles.noContentImage}
+          />
+          <Text style={[styles.noContentTitle, { color }]}>
+            На этот день лекарства не запланированы
+          </Text>
+          <Text style={[styles.noContentSubtitle, { color: mutedColor }]}>
+            Если вы ещё не добавили лекарство, сделайте это сейчас.
+          </Text>
+
+          <CustomButton
+            label="Добавить лекарства"
+            svgIcon={<PlusIcon size={15} />}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     paddingLeft: 20,
@@ -54,5 +78,28 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto_400Regular",
     fontSize: 16,
     lineHeight: 19.2,
+  },
+  noContentWrapper: {
+    flex: 1,
+    gap: 20,
+    alignItems: "center",
+  },
+  noContentImage: {
+    width: 160,
+    height: 160,
+  },
+  noContentTitle: {
+    width: 250,
+    fontFamily: "Roboto_500Medium",
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: "center",
+  },
+  noContentSubtitle: {
+    width: 250,
+    fontFamily: "Roboto_500Medium",
+    fontSize: 14,
+    lineHeight: 16.2,
+    textAlign: "center",
   },
 });
